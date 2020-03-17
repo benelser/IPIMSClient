93d7cc8fbb8d2521aaac577bb78e5457013338bf
Benjamin Elser <Benjamin.elser18@gmail.com>
2020-03-17 13:20:58 -0500

    updated requirements install

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ function check () {
     # Check for installs
     if which geckodriver; then CHECKS=$((CHECKS+=1)); fi;
     if dpkg --list firefox; then CHECKS=$((CHECKS+=1)); fi;
-    if dpkg --list python3-pip; then CHECKS=$((CHECKS+=1)); fi;
+    if dpkg --list python3-pip; then CHECKS=$((CHECKS+=1)); sudo python3 -m pip install -r requirements.txt; fi;
     if [ "$CHECKS" -ne 3 ] ; then echo "1"; else echo "0"; fi
 }
 
@@ -17,7 +17,7 @@ if [ "$INSTALL" -eq 1 ]; then
     # check for IPIMS_Client.py dependencies
     if which geckodriver; then echo "Driver installed"; else wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz -O /tmp/geckodriver.tar.gz && sudo tar -C /opt -xzf /tmp/geckodriver.tar.gz && sudo chmod 755 /opt/geckodriver && sudo ln -fs /opt/geckodriver /usr/bin/geckodriver && sudo ln -fs /opt/geckodriver /usr/local/bin/geckodriver; fi
     if dpkg --list firefox; then echo "Firefox installed"; else sudo apt-get install firefox xvfb -y; fi
-    if dpkg --list python3-pip; then echo "python3-pip installed"; else sudo apt-get install python3-pip -y; fi
+    if dpkg --list python3-pip; then echo "python3-pip installed"; sudo python3 -m pip install -r requirements.txt;; else sudo apt-get install python3-pip -y;sudo python3 -m pip install -r requirements.txt; fi
     check
 else
     check
